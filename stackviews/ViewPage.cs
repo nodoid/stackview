391f1638337d622417e2c25b97d0b0d3f53d3a58
Paul Johnson <paul@snuberry-software.co.uk>
2015-11-27 23:59:33 +0000

    initial import

---
+++ stackviews/ViewPage.cs
@@ -0,0 +1,49 @@
+﻿using Xamarin.Forms;
+
+namespace stackviews
+{
+    public class ViewPage : ContentPage
+    {
+        public ViewPage()
+        {
+            AbsoluteLayout stack = null;
+
+            var image = new Image
+            {
+                Source = "corrosive.png",
+                HorizontalOptions = LayoutOptions.CenterAndExpand,
+                VerticalOptions = LayoutOptions.CenterAndExpand
+            };
+            var tapImage = new TapGestureRecognizer
+            {
+                NumberOfTapsRequired = 1,
+                Command = new Command((t) =>
+                    {
+                        var view = new ViewInfo();
+                        var layout = new AbsoluteLayout()
+                        {
+                            VerticalOptions = LayoutOptions.CenterAndExpand,
+                            HorizontalOptions = LayoutOptions.CenterAndExpand,
+                            Children = { view }
+                        };
+                        stack.Children.Add(layout);
+                    })
+            };
+            image.GestureRecognizers.Add(tapImage);
+
+            stack = new AbsoluteLayout
+            { 
+                HorizontalOptions = LayoutOptions.CenterAndExpand,
+                VerticalOptions = LayoutOptions.CenterAndExpand,
+                Children =
+                {
+                    image
+                }
+            };
+
+            Content = stack;
+        }
+    }
+}
+
+
---
+++ stackviews/viewInfo.cs
@@ -0,0 +1,45 @@
+﻿using Xamarin.Forms;
+
+namespace stackviews
+{
+    public class ViewInfo : ContentView
+    {
+        public ViewInfo()
+        {
+            Content = new StackLayout
+            {
+                WidthRequest = 300,
+                HeightRequest = 300,
+                Opacity = .75,
+                BackgroundColor = Color.Silver,
+                Orientation = StackOrientation.Vertical,
+                VerticalOptions = LayoutOptions.FillAndExpand,
+                HorizontalOptions = LayoutOptions.CenterAndExpand,
+                Padding = new Thickness(12, 12),
+                Children =
+                {
+                    new Label
+                    {
+                        Text = "Hello John",
+                        TextColor = Color.Red
+                    },
+                    new Label
+                    {
+                        Text = "Got a new motor?",
+                        TextColor = Color.Blue,
+                    },
+                    new Button
+                    {
+                        Text = "Close",
+                        Command = new Command((t) =>
+                            {
+                                Content = null; // "removes" the view
+                            })
+                    }
+                }
+            };
+        }
+    }
+}
+
+
